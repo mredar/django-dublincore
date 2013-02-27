44a8af6670c8e181bbcc44b995c84b4d81b7bcbb
Mark Redar <mredar@gmail.com>
2013-02-27 14:19:48 -0800

    Update some metadata.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name = 'django-dublincore',
-    version = '0.0',
+    version = '0.1',
     packages = ['dublincore'],
     include_package_data = True,
     license = 'BSD License - see LICENSE file', 
@@ -17,6 +17,7 @@ setup(
     author = 'Mark Redar',
     author_email = 'mredar@gmail.com',
     classifiers = [
+        'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
